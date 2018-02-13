b98f4f6375db5152c83660f88e893fb73fdf3086
«simenkf» <simen_kf@hotmail.com>
2018-02-13 20:15:49 +0100

    Module for communication with the elevator driver created. Funtioning properly with the simulator. TODO: create safe guards cheking for valid input-commands

--- driver_communication.erl
+++ driver_communication.erl
@@ -1,36 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% This module takes care of communication with the elevator     %%
+%% driver over TCP. A loop-function is spawned, which is always  %%
+%% available for receiving requests about reading from and       %%
+%% writing to the elevator hardware. The function loop runs      %%
+%% forever whereas start retuns the PID for loop.                %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 -module(driver_communication).
 -export([start/0]).
--define(NumberOfFloors, 4).
+-include("parameters.hrl").
 
 start() ->
-	{ok, Socket} = gen_tcp:connect({127,0,0,1}, 15657, [binary, {active, false}]),
-	PID = spawn(fun()-> loop(Socket) end),
-
-	io:format("Spawn succesfull"),
-	timer:sleep(2000),
-
-	test(PID),
-
-	terminated_fine.
-
-
-test(PID) ->
-	PID ! {self(), get_stop_button, 0, 0, 0},
-	test(PID).
+	{ok, Socket} = gen_tcp:connect(localhost, 15657, [binary, {active, false}]),
+	spawn(fun()-> loop(Socket) end).
 
 loop(Socket) ->
 	receive
-		{motor_dir, up} ->
-			gen_tcp:send(Socket, [1, 1, 0, 0]);
-		{motor_dir, down} ->
-			gen_tcp:send(Socket, [1, 255, 0, 0]);
-		{motor_dir, stop} ->
-			gen_tcp:send(Socket, [1, 0, 0, 0]);
+		{motor_dir, Dir} ->
+			set_motor_dir_func(Socket, Dir);
 
-		{order_button_LED, Button_type, Floor, Value} -> %when
-		%lists:member(Button_type, [0, 1, 2]),
-		%lists:member(Floor, lists:seq(0, ?NumberOfFloors)),
-		%lists:member(Value, [0, 1]) ->
+		{order_button_LED, Button_type, Floor, Value} -> %when %lists:member(Button_type, [0, 1, 2]), %lists:member(Floor, lists:seq(0, ?NumberOfFloors)), %lists:member(Value, [0, 1]) ->
 			gen_tcp:send(Socket, [2, Button_type, Floor, Value]);
 
 		{door_open_LED, Value} when Value =:= 0 ; Value =:= 1 ->
@@ -40,37 +29,59 @@ loop(Socket) ->
 			gen_tcp:send(Socket, [5, Value, 0, 0]);
 
 		{PID, get_order_button, Button_type, Floor} -> %add guard
-			gen_tcp:send(Socket, [6, Button_type, Floor, 0]),
-			case gen_tcp:recv(Socket, 4, 2000) of
-				{ok, Answer} ->
-					io:format("~w", [Answer]),
-					io:format("~s", [Answer]);
-				{error, Reason} ->
-					io:format("~s", [Reason])
-			end;
+			get_order_button_func(Socket, PID, Button_type, Floor);
 
 		{PID, get_floor} -> %add guard
-			gen_tcp:send(Socket, [7, 0, 0, 0]),
-			case gen_tcp:recv(Socket, 4, 2000) of
-				{ok, Answer} ->
-					io:format("~w", [Answer]),
-					io:format("~s", [Answer]);
-				{error, Reason} ->
-					io:format("~s", [Reason])
-			end;
+			get_floor_func(Socket, PID);
 		
 		{PID, get_stop_button} -> %add guard
-			gen_tcp:send(Socket, [8, 0, 0, 0]),
-			case gen_tcp:recv(Socket, 4, 2000) of
-				{ok, Answer} ->
-					io:format("~w", [Answer]),
-					io:format("~s", [Answer]);
-				{error, Reason} ->
-					io:format("~s", [Reason])
-			end
-
-	after
-		2000 ->
-			io:format("Timeout~n")
+			get_stop_button_func(Socket, PID);
+		
+		Unexpected ->
+			io:format("unexpected message: ~p~n", [Unexpected])
 	end, 
 	loop(Socket).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Help functions taking care og the actual communication with   %%
+%% the elevator driver. Where applicable it also send in return  %%
+%% the corresponding data to the procces asking for information. %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+set_motor_dir_func(Socket, up) ->
+	gen_tcp:send(Socket, [1, 1, 0, 0]);
+set_motor_dir_func(Socket, down) ->
+	gen_tcp:send(Socket, [1, 255, 0, 0]);
+set_motor_dir_func(Socket, stop) ->
+	gen_tcp:send(Socket, [1, 0, 0, 0]);
+set_motor_dir_func(_, _) ->
+	io:format("ERROR: invalid direction!"),
+	error.
+
+get_order_button_func(Socket, PID, Button_type, Floor) ->
+	gen_tcp:send(Socket, [6, Button_type, Floor, 0]),
+		case gen_tcp:recv(Socket, 4, 2000) of
+			{ok, Answer} ->
+				PID ! {get_order_button, Button_type, Floor, Answer};
+			{error, Reason} ->
+				PID ! {error, Reason}
+		end.
+
+get_floor_func(Socket, PID) ->
+	gen_tcp:send(Socket, [7, 0, 0, 0]),
+		case gen_tcp:recv(Socket, 4, 2000) of
+			{ok, Answer} ->
+				PID ! {get_floor, Answer};
+			{error, Reason} ->
+				PID ! {error, Reason}
+		end.
+
+get_stop_button_func(Socket, PID) ->
+	gen_tcp:send(Socket, [8, 0, 0, 0]),
+		case gen_tcp:recv(Socket, 4, 2000) of
+			{ok, Answer} ->
+				PID ! {get_stop_button, Answer};
+			{error, Reason} ->
+				PID ! {error, Reason}
+		end.
