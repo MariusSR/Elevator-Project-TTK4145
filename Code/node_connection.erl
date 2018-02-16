0109c8f239817ef82a6ccdc4ff9e42fa0ba46ad1
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-02-16 16:24:43 +0100

    changed how get_IP() works. Now functioning properly-ish, needs testing. Also edited listen_for_nodes(), now having more pattern matches as well as a necessary match on everything. OS:cmd deamin seems ncesseary, dont know why yet

--- Code/node_connection.erl
+++ Code/node_connection.erl
@@ -10,42 +10,45 @@
 -module(node_connection).
 -export([start/0]).
 
--define(RECEIVE_PORT, 1234).
+-define(RECEIVE_PORT, 5679).
 -define(BROADCAST_PORT, 5678).
 -define(TIMEOUT, 2000).
 -define(BROADCAST_SLEEP, 5000).
+-define(TICKTIME, 1000).
 
 start() ->
 	init_node_cluster(),
 	spawn(fun() -> broadcast_self() end),
 	spawn(fun() -> listen_for_nodes() end),
-	io:format("Now finding new nodes to add to the cluster."),
+	io:format("Now finding new nodes to add to the cluster.~n"),
 	ok.
 
 %%% Start node cluster with coockie
 init_node_cluster() ->
+	os:cmd("epmd -daemon"), %% check what this is
+	%timer:sleep(100), % perhaps needed
 	ThisNode = list_to_atom("elevator@" ++ get_IP()),
-	{ok, pid} = net_kernel:start(ThisNode, longnames),
-	erlang:set_cookie(ThisNode, 'top_secret').					%%TODO: Make the cookie an atom
+	{ok, _Pid} = net_kernel:start([ThisNode, longnames, ?TICKTIME]),
+	erlang:set_cookie(ThisNode, 'top_secret').	%%TODO: Make the cookie an atom
 
 %%% Get local IP address on format 123.456.789.012
 get_IP() ->
-	{A, B, C, D} = inet:ip4_address(),
-	A ++ "." ++ B ++ "." ++ C ++ "." ++ D.
+	{ok, [{addr, IP}]} = inet:ifget("en0", [addr]), %% check getiflist()!!!!
+	inet_parse:ntoa(IP).
 
 %%% Broadcast its own node name
 broadcast_self() ->
-	{ok, BroadcastSocket} = gen_udp:open(?BROADCAST_PORT, [binary, {broadcast, true}]),
+	{ok, BroadcastSocket} = gen_udp:open(?BROADCAST_PORT, [list, {broadcast, true}]),
 	broadcast_self(BroadcastSocket).
 
 broadcast_self(BroadcastSocket) ->
-	gen_udp:send(BroadcastSocket, {255, 255, 255, 255}, ?RECEIVE_PORT, node()),
+	gen_udp:send(BroadcastSocket, {255, 255, 255, 255}, ?RECEIVE_PORT, atom_to_list(node())),
 	timer:sleep(?BROADCAST_SLEEP),
 	broadcast_self(BroadcastSocket).
 
 %%% Listen for new nodes to connect
 listen_for_nodes() ->
-	{ok, ReceiveSocket} = gen_udp:open(?RECEIVE_PORT, [binary, {active, false}]),
+	{ok, ReceiveSocket} = gen_udp:open(?RECEIVE_PORT, [list, {active, false}]),
 	listen_for_nodes(ReceiveSocket).
 
 listen_for_nodes(ReceiveSocket) ->
@@ -55,10 +58,15 @@ listen_for_nodes(ReceiveSocket) ->
 			case lists:member(Node, [node()|nodes()]) of
 				false ->
 					net_kernel:connect_node(Node),
-					io:format("New node connected: ~w~n", [Node])
+					io:format("New node connected: ~w~n", [Node]);
+				_ -> 
+					ok
 			end;
 
+		{error, timeout} ->
+			ok;
+
 		{error, Reason} ->
-			io:format("ERROR: ~s~n", [Reason])
+			io:format("ERROR: receiving node failed due to: ~s~n", [Reason])
 	end,
 	listen_for_nodes(ReceiveSocket).
