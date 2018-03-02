8925bd47e5de30314b4015ef2d055d76cc156b1a
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-02 15:03:35 +0100

    changed "_" to "Unexpected"

--- src/button_reader.erl
+++ src/button_reader.erl
@@ -36,8 +36,8 @@ when is_integer(Floor) andalso Floor > 1 andalso Floor < ?NUMBER_OF_FLOORS andal
             ok;
         {error, Reason} ->
             io:format("ERROR: receiving button status for button type ~p on floor ~p failed due to: ~s~n", [Button_type, Floor, Reason]);
-        _ ->
-            io:format("Unknown message received for button type ~p on floor ~p\n", [Button_type, Floor])
+        Unexpected ->
+            io:format("Unexpexted message received for button type ~p on floor ~p: ~p\n", [Button_type, Floor, Unexpected])
     after
         ?TIMEOUT ->
             io:format("Timeout in button reader module\n")
