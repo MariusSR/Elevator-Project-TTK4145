b5b3fdd660b8d415b516998a7dc453be5e7a83bd
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-03 23:14:57 +0100

    changed my mind :P Now only added reccursion to Unexpected and clear_queue. Also removed burly brackets from clear_queue-command

--- src/order_manager.erl
+++ src/order_manager.erl
@@ -74,15 +74,11 @@ node_communication(LocalOrderList) ->
             node_communication(LocalOrderList);
 
         % Function for debug use only, to be removed!
-        {clear_queue} ->
-            lists:foreach(fun(Order) -> {order_manager, hd(nodes())} ! {order_finished_no_reccursion, Order} end , LocalOrderList),
+        clear_queue ->
+            lists:foreach(fun(Order) -> {order_manager, hd(nodes())} ! {order_finished, Order} end , LocalOrderList),
             node_communication(LocalOrderList);
 
-        {order_finished_no_reccursion, Order} when is_tuple(Order) ->
-            io:format("Received: order_finished\n"),
-            lists:foreach(fun(Node) -> {order_manager, Node} ! {remove_order, Order, LocalOrderList} end, [node()|nodes()]);
-
         Unexpected ->
-			io:format("Unexpected message in order_managers node_communication: ~p~n", [Unexpected]),
+	    io:format("Unexpected message in order_managers node_communication: ~p~n", [Unexpected]),
             node_communication(LocalOrderList)
     end.
