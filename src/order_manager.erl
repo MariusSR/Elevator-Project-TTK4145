7687c23b4e569efabdeca6e1b1fa3f071683b5e5
«simenkf» <simen_kf@hotmail.com>
2018-03-06 23:51:33 +0100

    minor changes, added some comments

--- src/order_manager.erl
+++ src/order_manager.erl
@@ -39,7 +39,7 @@ main_loop(Orders, Elevator_states) ->
         %% Removes all occourences of a cab order from 'cab_orders' of Orders
         {remove_order, {cab_button, Floor}} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
             Updated_cab_orders = [X || X <- Orders#orders.cab_orders, X /= Floor],
-            Updated_orders = Orders#orders{cab_orders = Updated_cab_orders},
+            Updated_orders     = Orders#orders{cab_orders = Updated_cab_orders},
             main_loop(Updated_orders, Elevator_states);
 
         %% Removes all occourences of a hall order from 'unassigned' and 'assigned' of Orders
@@ -54,11 +54,15 @@ main_loop(Orders, Elevator_states) ->
             Updated_states = dict:store(Node, New_state, Elevator_states),
             main_loop(Orders, Updated_states);
 
-        %% Checks is the elevator should stop at Floor
+        %% Checks is the elevator should stop at Floor when moving up
         {should_i_stop_at, Floor, up_dir}   when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
             lists:member({up_button, Floor},   Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders);
+        
+        %% Checks is the elevator should stop at Floor when moving down
         {should_i_stop_at, Floor, down_dir} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
             lists:member({down_button, Floor}, Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders);
+        
+        %% Asked to stop when already stopped; should never happen, but return true just in case
         {should_i_stop_at, _Floor, stop_dir} ->
             io:format("Error in order_manager, asked should_i_stop when already in state stoped\n"),
             true
