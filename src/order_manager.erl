dc88bdae571e9cd6695513ee9dca23d62001cf57
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-07 15:42:23 +0100

    Renamed file, changed an atom and updated comments

--- src/order_manager.erl
+++ src/order_manager.erl
@@ -13,7 +13,10 @@ start() ->
 main_loop(Orders, Elevator_states) ->
     io:format("Main loop of order_manager/scheduler started\n"),
     receive
-        %% Acknowledge the cab order and append it to 'cab_orders' of Orders if not already known
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Acknowledge the order and append it to correspoding list of 'Orders' if not already present
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Cab orders
         {add_order, {cab_button, Floor}, PID} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
             PID ! {ack_order, {cab_button, Floor}},
             case lists:member(Floor, Orders#orders.cab_orders) of
@@ -23,8 +26,7 @@ main_loop(Orders, Elevator_states) ->
                     Updated_orders = Orders#orders{cab_orders = Orders#orders.cab_orders ++ [Floor]},
                     main_loop(Updated_orders, Elevator_states)
             end;
-
-        %% Acknowledge the hall order and append it to 'unassigned' of Orders if not already known
+        %% Hall orders
         {add_order, {Button_type, Floor}, PID} when is_atom(Button_type) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
             Hall_order = {Button_type, Floor},
             PID ! {ack_order, Hall_order},
@@ -36,13 +38,15 @@ main_loop(Orders, Elevator_states) ->
                     main_loop(Updated_orders, Elevator_states)
             end;
 
-        %% Removes all occourences of a cab order from 'cab_orders' of Orders
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Removes all occourences of an order from Orders
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Cab orders
         {remove_order, {cab_button, Floor}} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
             Updated_cab_orders = [X || X <- Orders#orders.cab_orders, X /= Floor],
             Updated_orders     = Orders#orders{cab_orders = Updated_cab_orders},
             main_loop(Updated_orders, Elevator_states);
-
-        %% Removes all occourences of a hall order from 'unassigned' and 'assigned' of Orders
+        %% Hall orders
         {remove_order, {Button_type, Floor}} when is_atom(Button_type) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
             Updated_unassigned_hall_orders = [X || X <- Orders#orders.unassigned_hall_orders, X /= {Button_type, Floor}],
             Updated_assigned_hall_orders   = [X || X <- Orders#orders.assigned_hall_orders,   X /= {Button_type, Floor}],
@@ -50,22 +54,28 @@ main_loop(Orders, Elevator_states) ->
                                              assigned_hall_orders = Updated_assigned_hall_orders},
             main_loop(Updated_orders, Elevator_states);
 
-        %% Update state of Node in Elevator_states, adding it if non-existing
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Update state of 'Node' in 'Elevator_states', adding it if not present
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         {update_state, Node, New_state} when is_atom(Node) andalso is_record(New_state, state) ->
             Updated_states = dict:store(Node, New_state, Elevator_states),
             main_loop(Orders, Updated_states);
 
-        %% Checks is the elevator should stop at Floor when moving up
-        {should_i_stop_at, Floor, up_dir}   when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-            lists:member({up_button, Floor},   Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders);
-        
-        %% Checks is the elevator should stop at Floor when moving down
-        {should_i_stop_at, Floor, down_dir} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-            lists:member({down_button, Floor}, Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders);
-        
-        %% Asked to stop when already stopped; should never happen, but return true just in case
-        {should_i_stop_at, _Floor, stop_dir} ->
-            io:format("Error in order_manager, asked should_i_stop when already in state stoped\n"),
-            true
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Checks is the elevator should stop at 'Floor' when moving in the specified direction
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Moving up
+        {should_elevator_stop, Floor, up_dir, PID}   when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
+            lists:member({up_button, Floor},   Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders),
+            main_loop(Orders, Elevator_states);
+        %% Moving down
+        {should_elevator_stop, Floor, down_dir, PID} when Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
+            PID ! lists:member({down_button, Floor}, Orders#orders.unassigned_hall_orders) or lists:member(Floor, Orders#orders.cab_orders),
+            main_loop(Orders, Elevator_states);
+        %% Already stoped, should never happen
+        {should_elevator_stop, _Floor, stop_dir, PID} when is_pid(PID) ->
+            io:format("Unexpected behaviour in order_manager, asked should_stop when already in state stoped\n"),
+            PID ! true,
+            main_loop(Orders, Elevator_states)
 
     end.
