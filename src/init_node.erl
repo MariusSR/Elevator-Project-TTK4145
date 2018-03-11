81e8147c587d1716c5c9dba8ff1a7e7cde5a13a1
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-11 01:55:21 +0100

    Fixed some issues. Now turning on LEDs in simualtor with >=2 elevators. However, it does not move to the orders. Also; cab order LEDs are not turned on when only one elevator present.

--- src/init_node.erl
+++ src/init_node.erl
@@ -7,14 +7,14 @@ start() ->
     register(driver, spawn(fun()-> driver_communication:start() end)),
     io:format("driver PID: ~p\n", [whereis(driver)]),
 
-    register(order_manager, spawn(fun()-> order_manager:node_communicator() end)),
+    register(order_manager, spawn(fun()-> order_manager:start() end)),
     io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
 
     register(fsm, spawn(fun()-> fsm:start() end)),
     io:format("FSM PID: ~p\n", [whereis(fsm)]),
 
-    register(node_communicator, spawn(fun()-> node_communicator:node_communicator() end)),
-    io:format("Node_cimmunicator PID: ~p\n", [whereis(node_communicator)]),
+    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
+    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
 
     A = spawn(fun()-> button_reader:read_button_loop(1) end),
     io:format("button_reader PID: ~p\n", [A]),
@@ -29,11 +29,14 @@ start_local() ->
     register(driver, spawn(fun()-> driver_communication:start() end)),
     io:format("driver PID: ~p\n", [whereis(driver)]),
 
-    register(order_manager, spawn(fun()-> order_manager:node_communication() end)),
+    register(order_manager, spawn(fun()-> order_manager:start() end)),
     io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
 
     register(fsm, spawn(fun()-> fsm:start() end)),
     io:format("FSM PID: ~p\n", [whereis(fsm)]),
+
+    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
+    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
 
     A = spawn(fun()-> button_reader:read_button_loop(1) end),
     io:format("button_reader PID: ~p\n", [A]),
@@ -46,11 +49,14 @@ start_local(Port) ->
     register(driver, spawn(fun()-> driver_communication:start(Port) end)),
     io:format("driver PID: ~p\n", [whereis(driver)]),
 
-    register(order_manager, spawn(fun()-> order_manager:node_communication() end)),
+    register(order_manager, spawn(fun()-> order_manager:start() end)),
     io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
 
     register(fsm, spawn(fun()-> fsm:start() end)),
     io:format("FSM PID: ~p\n", [whereis(fsm)]),
+
+    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
+    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
 
     A = spawn(fun()-> button_reader:read_button_loop(1) end),
     io:format("button_reader PID: ~p\n", [A]),
