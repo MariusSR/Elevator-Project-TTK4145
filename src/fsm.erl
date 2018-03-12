5b0c7984b6d6c76c5bd15b2fe15436fac2e8e8e5
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-12 08:14:59 +0100

    Fix bug in fsm

--- src/fsm.erl
+++ src/fsm.erl
@@ -22,9 +22,9 @@ fsm(idle, Latest_floor) ->
     fsm(idle_loop, Latest_floor);
 
 fsm(idle_loop, Latest_floor) ->
-    order_manager ! get_unassigned_order, 
+    order_manager ! {get_unassigned_order, self()}, 
     %marius ! is_idle,         %%DEGUB
-    io:format("ER I IDLE OG VENTER PÅ ORDRE!\n"),
+    %io:format("ER I IDLE OG VENTER PÅ ORDRE!\n"),
     receive 
         {Button_type, Floor} when is_atom(Button_type) andalso Floor =< ?NUMBER_OF_FLOORS andalso Floor >= 1 ->
             Order = {Button_type, Floor},
