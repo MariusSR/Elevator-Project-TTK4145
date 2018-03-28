d97cf8f0e3fc9518a2b3a38a38dea0c08ec268fe
«simenkf» <simen_kf@hotmail.com>
2018-03-28 16:17:50 +0200

    first real attempt on getting existing hall orders at bootup

--- src/init_node.erl
+++ src/init_node.erl
@@ -1,5 +1,5 @@
 -module(init_node).
--export([start/0, start_local/0, start_local/1]).
+-export([start/0, start_local/1]).
 
 start() -> 
     node_connection:start(),
@@ -7,39 +7,20 @@ start() ->
     register(driver, spawn(fun() -> driver_communication:start() end)),
     io:format("driver PID: ~p\n", [whereis(driver)]),
 
+    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
+    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
+
     register(order_manager, spawn(fun() -> order_manager:start() end)),
     io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
 
     register(fsm, spawn(fun()-> fsm:start() end)),
     io:format("FSM PID: ~p\n", [whereis(fsm)]),
 
-    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
-    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
-
     A = spawn(fun()-> button_reader:read_button_loop(1) end),
     io:format("button_reader PID: ~p\n", [A]),
 
     io:format("Start completed\n\n").
 
-start_local() ->
-     %node_connection:start(),
-     
-    register(driver, spawn(fun()-> driver_communication:start() end)),
-    io:format("driver PID: ~p\n", [whereis(driver)]),
-
-    register(order_manager, spawn(fun()-> order_manager:start() end)),
-    io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
-
-    register(fsm, spawn(fun()-> fsm:start() end)),
-    io:format("FSM PID: ~p\n", [whereis(fsm)]),
-
-    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
-    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
-
-    A = spawn(fun()-> button_reader:read_button_loop(1) end),
-    io:format("button_reader PID: ~p\n", [A]),
-
-    io:format("Start completed\n\n").
 
 start_local(Port) ->
      %node_connection:start(),
@@ -47,14 +28,14 @@ start_local(Port) ->
     register(driver, spawn(fun()-> driver_communication:start(Port) end)),
     io:format("driver PID: ~p\n", [whereis(driver)]),
 
+    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
+    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
+
     register(order_manager, spawn(fun()-> order_manager:start() end)),
     io:format("ordermanager PID: ~p\n", [whereis(order_manager)]),
 
     register(fsm, spawn(fun()-> fsm:start() end)),
     io:format("FSM PID: ~p\n", [whereis(fsm)]),
-
-    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
-    io:format("Node_communicator PID: ~p\n", [whereis(node_communicator)]),
 
     A = spawn(fun()-> button_reader:read_button_loop(1) end),
     io:format("button_reader PID: ~p\n", [A]),
--- src/node_communicator.erl
+++ src/node_communicator.erl
@@ -15,6 +15,7 @@
 
 % TODO: spawn from gloabl spawner, remember there is only coments for LED now
 % TODO: remove unnecessary comments
+% endre rekkefølgen på receivene, samle alle med order feks
 
 start() ->
     io:format("Main loop of node_communicator started\n"),
@@ -23,64 +24,56 @@ start() ->
 main_loop() ->
     receive
         {new_order, {cab_button, Floor}} ->
-            %io:format("Received: new_cab_order~p\n", [{cab_button, Floor}]),
             node_communicator ! {add_order, {cab_button, Floor}, node()};
 
         {new_order, Order} when is_tuple(Order) ->
-            %io:format("Received: new_hall_order~p\n", [Order]),
             lists:foreach(fun(Node) -> {node_communicator, Node} ! {add_order, Order, node()} end, nodes());
 
         {add_order, Order, From_node} when is_tuple(Order) andalso is_atom(From_node) ->
-            %io:format("Received: add_order~p\n", [Order]),
             order_manager ! {add_order, Order, From_node};
 
         {order_added, Order, From_node} when is_tuple(Order) andalso is_atom(From_node)  ->
-            %io:format("Received: order_added\n"),
             {node_communicator, From_node} ! {ack_order, Order};
         
         {ack_order, {cab_button, Floor}} ->
-            %io:format("Received: ack_cab_order\n"),
             order_manager ! {add_order, {cab_button, Floor}, node()},
             node_communicator ! {set_order_button_LED, on, {cab_button, Floor}};
 
         {ack_order, Order} when is_tuple(Order) ->
-            %io:format("Received: ack_hall_order\n"),
             order_manager ! {add_order, Order, node()},
             lists:foreach(fun(Node) -> {node_communicator, Node} ! {set_order_button_LED, on, Order} end, [node()|nodes()]);
         
         {new_order_assigned, Order} when is_tuple(Order) ->
-            io:format("Received: new_order_assigned~p\n", [Order]),
             lists:foreach(fun(Node) -> {node_communicator, Node} ! {mark_order_assigned, Order, node()} end, [node()|nodes()]);
 
         {mark_order_assigned, Order, Node} when is_tuple(Order) ->
-            io:format("Received: mark_order_assigned~p\n", [Order]),
             order_manager ! {mark_order_assigned, Order, Node};
         
         {set_order_button_LED, on, {Button_type, Floor}} when is_atom(Button_type) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-            %io:format("Received: LED_on\n"),
             driver ! {set_order_button_LED, Button_type, Floor, on};
 
         {set_order_button_LED, off, {Button_type, Floor}} when is_atom(Button_type) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-            %io:format("Received: LED_off\n"),
             driver ! {set_order_button_LED, Button_type, Floor, off};
         
         {order_finished, Order} when is_tuple(Order) ->
             % FORVENTER NÅ Å MOTTA ALLE ORDRE SOM ER FULLFØRT I TUR OG ORDEN, DVS FSM MÅ BÅDE SENDE CAB OG HALL SOM FULLFØRT ETTER HVERANDRE
-            %io:format("Received: order_finished\n"),
             lists:foreach(fun(Node) -> {node_communicator, Node} ! {clear_order, Order} end, [node()|nodes()]);
 
         {clear_order, {Button_type, Floor}} when is_atom(Button_type) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-            %io:format("Received: clear_order\n"),
             order_manager ! {remove_order, {Button_type, Floor}},
             driver ! {set_order_button_LED, Button_type, Floor, off};
         
         {reached_new_state, State} when is_record(State, state) ->
-            %io:format("Received: new_state\n"),
             lists:foreach(fun(Node) -> {node_communicator, Node} ! {update_state, node(), State} end, [node()|nodes()]);
 
         {update_state, Node, New_state} when is_atom(Node) andalso is_record(New_state, state) ->
-            %io:format("Received: update_state\n"),
             order_manager ! {update_state, Node, New_state};
+
+        {sync_hall_orders_with_new_node, New_node, Assigned_hall_orders, Unassigned_hall_orders} ->
+            {node_communicator, New_node} ! {existing_hall_orders, Assigned_hall_orders, Unassigned_hall_orders};
+        
+        {existing_hall_orders, Assigned_hall_orders, Unassigned_hall_orders} ->
+            order_manager ! {existing_hall_orders, Assigned_hall_orders, Unassigned_hall_orders};
 
         %%% FOR DEBUG ONLY %%%
         reset ->
