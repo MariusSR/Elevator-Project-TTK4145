b41c1d8962d37f3db0608082de7cea5c30438ad8
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-03-28 18:02:00 +0200

    still not working properly, added sleep

--- src/node_connection.erl
+++ src/node_connection.erl
@@ -24,7 +24,7 @@ start() ->
 	spawn(fun() -> broadcast_self() end),
 	spawn(fun() -> listen_for_nodes() end),
 	spawn(fun() -> start_node_monitoring() end),
-	%timer:sleep(1000), % wait for connection 
+	timer:sleep(1000), % wait for connection 
 	io:format("Node cluster initialized, now searching for friends.~n").
 
 %--------------------------------------------------------------------------------------------------
@@ -95,6 +95,7 @@ node_monitoring_loop() ->
 	receive
 		{nodeup, New_node} ->
 			io:format("New node connected: ~p\n", [New_node]),
+			timer:sleep(1000), %wait for new node to spwan properly
 			order_manager ! {node_up, New_node};
 
 		{nodedown, Node} ->
