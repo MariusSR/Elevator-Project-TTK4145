43c087151d9d84a1ad518968712e549de01c76eb
«simenkf» <simen_kf@hotmail.com>
2018-03-31 14:48:52 +0200

    Started on watchdog timer. Parts only pseudocode as of now.

---
+++ src/watchdog.erl
@@ -0,0 +1,33 @@
+-module(watchdog).
+-export([start/0]).
+-define(TIME_LIMIT, 10000).
+
+start() ->
+    main_loop([]).
+
+main_loop(Watch_list) -> %watch_list er en liste av ordre med tilhørende PID for prosess som timer den, altå [{PID, Order}, ...]
+    receive
+        {start_watching, Order} ->
+            PID = spawn(fun() -> watchdog_timer(Order) end),
+            main_loop(Watch_list ++ {PID, Order});
+        
+        {stop_watching, Order} -> % tenker denne skal kalles når en ordre blir utført, uavhengig om den watches eller ikke. cab og hall sendes hver for seg, som 2 kall
+            PID = 1, % her må PID finnes fra watch_list, nå bare en dummy-verdi. bruke case of for å sjekke om PIDen faktisk finnes, altså om ordren er i watch_listen
+            PID ! order_finished, %kun hvis den finnes
+            main_loop(Watch_list -- {PID, Order}) %kun hvis den finnes (men kan gjøres uansett da)
+        
+        {timed_out, PID, Order} ->
+            main_loop(watch_list -- {PID, Order})            
+    end,
+
+watchdog_timer(Order) ->
+    receive
+        order_finished ->
+            ok;
+    after
+        ?TIME_LIMIT ->
+            io:format("Order timed out!\n"),
+            order_manager ! {unassign_order, Order}, %denne må lages i order_manager
+            watchdog ! {timed_out, self(), Order}
+    end.
+        
