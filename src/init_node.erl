75114109df5f43f08373d7b902767aceae1be287
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-04-10 18:37:13 +0200

    BIG CHANGE: new IP used

--- src/init_node.erl
+++ src/init_node.erl
@@ -1,5 +1,5 @@
 -module(init_node).
--export([start/0, start_local/1]).
+-export([start/0]).
 
 start() ->
     timer:sleep(100),
@@ -25,25 +25,4 @@ start() ->
 
     button_reader:start(),
 
-    io:format("\n\n").
-
-
-start_local(Port) ->     
-    register(driver, spawn(fun()-> driver_interface:start(Port) end)),
-    io:format("~s~p\n", [color:cyan("Driver PID:              "), whereis(driver)]),
-
-    register(node_communicator, spawn(fun()-> node_communicator:start() end)),
-    io:format("~s~p\n", [color:cyan("Node_communicator PID:   "), whereis(node_communicator)]),
-
-    register(order_manager, spawn(fun()-> order_manager:start() end)),
-    io:format("~s~p\n", [color:cyan("Ordermanager PID:        "), whereis(order_manager)]),
-
-    register(watchdog, spawn(fun() -> watchdog:start() end)),
-    io:format("~s~p\n", [color:cyan("Watchdog PID:            "), whereis(watchdog)]),
-
-    register(fsm, spawn(fun()-> fsm:start() end)),
-    io:format("~s~p\n", [color:cyan("FSM PID:                 "), whereis(fsm)]),
-
-    button_reader:start(),
-    
     io:format("\n\n").
