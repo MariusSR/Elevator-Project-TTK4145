8235bde63d84eac3f6a25549cc78ad28f2b08230
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-04-11 13:01:02 +0200

    merged with old driver_interface
    For some reason this file wasn't merged correctly when merging new_fsm into master. Now resolved manually.

--- src/driver_interface.erl
+++ src/driver_interface.erl
@@ -1,12 +1,11 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% This module takes care of communication with the elevator driver over TCP. A      %%
-%% loop-function is spawned, which is always available for receiving requests about  %%
-%% reading from and writing to the elevator hardware. The function loop runs forever %%
-%% whereas start returns the PID for loop.              			   			     %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%=================================================================================================
+%% This module interfaces with the HW driver over TCP. The module is interfaced by messages sent to
+%% 'driver' received in the 'main_loop'. Some calls are handled by help functions found in the end.
+%%=================================================================================================
 
 -module(driver_interface).
 -export([start/0, start/1]).
+
 -include("parameters.hrl").
 -define(TIMEOUT, 2000).
 -define(MSG_LENGTH, 4).
@@ -21,81 +20,68 @@ start(Port) ->
 	initialize_LED(Socket),
 	main_loop(Socket).
 
+
+
+%--------------------------------------------------------------------------------------------------
+% Loop receiving calls to 'driver'
+%--------------------------------------------------------------------------------------------------
 main_loop(Socket) ->
 	receive
-		{set_motor_dir, up_dir} ->
-			gen_tcp:send(Socket, [1, 1, 0, 0]);
-		{set_motor_dir, stop_dir} ->
-			gen_tcp:send(Socket, [1, 0, 0, 0]);
-		{set_motor_dir, down_dir} ->
-			gen_tcp:send(Socket, [1, 255, 0, 0]);
+
+		{set_motor_dir, up_dir}   -> gen_tcp:send(Socket, [1, 1, 0, 0]);
+		{set_motor_dir, stop_dir} -> gen_tcp:send(Socket, [1, 0, 0, 0]);
+		{set_motor_dir, down_dir} -> gen_tcp:send(Socket, [1, 255, 0, 0]);
 		
-		{set_order_button_LED, up_button,   Floor, on}  when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 0, Floor - 1, 1]);
-		{set_order_button_LED, up_button,   Floor, off} when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 0, Floor - 1, 0]);
-		{set_order_button_LED, down_button, Floor, on}  when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 1, Floor - 1, 1]);
-		{set_order_button_LED, down_button, Floor, off} when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 1, Floor - 1, 0]);
-		{set_order_button_LED, cab_button,  Floor, on}  when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 2, Floor - 1, 1]);
-		{set_order_button_LED, cab_button,  Floor, off} when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS ->
-			gen_tcp:send(Socket, [2, 2, Floor - 1, 0]);
+		{set_order_button_LED, up_button,   Floor, on}  -> gen_tcp:send(Socket, [2, 0, Floor - 1, 1]);
+		{set_order_button_LED, up_button,   Floor, off} -> gen_tcp:send(Socket, [2, 0, Floor - 1, 0]);
+		{set_order_button_LED, down_button, Floor, on}  -> gen_tcp:send(Socket, [2, 1, Floor - 1, 1]);
+		{set_order_button_LED, down_button, Floor, off} -> gen_tcp:send(Socket, [2, 1, Floor - 1, 0]);
+		{set_order_button_LED, cab_button,  Floor, on}  -> gen_tcp:send(Socket, [2, 2, Floor - 1, 1]);
+		{set_order_button_LED, cab_button,  Floor, off} -> gen_tcp:send(Socket, [2, 2, Floor - 1, 0]);
 
-		{set_door_open_LED, on} ->
-			gen_tcp:send(Socket, [4, 1, 0, 0]);
-		{set_door_open_LED, off} ->
-			gen_tcp:send(Socket, [4, 0, 0, 0]);
+		{set_floor_LED, Floor} -> gen_tcp:send(Socket, [3, Floor - 1, 0, 0]);
 
-		{set_stop_button_LED, on} ->
-			gen_tcp:send(Socket, [5, 1, 0, 0]);
-		{set_stop_button_LED, off} ->
-			gen_tcp:send(Socket, [5, 0, 0, 0]);
+		{set_door_open_LED, on}  -> gen_tcp:send(Socket, [4, 1, 0, 0]);
+		{set_door_open_LED, off} -> gen_tcp:send(Socket, [4, 0, 0, 0]);
 
-		{set_floor_LED, Floor} ->
-			gen_tcp:send(Socket, [3, Floor - 1, 0, 0]);
+		{set_stop_button_LED, on}  -> gen_tcp:send(Socket, [5, 1, 0, 0]);
+		{set_stop_button_LED, off} -> gen_tcp:send(Socket, [5, 0, 0, 0]);
 
-		{get_order_button_status, up_button, Floor, PID}   when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
-			return_order_button_status(Socket, PID, 0, Floor - 1);
-		{get_order_button_status, down_button, Floor, PID} when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
-			return_order_button_status(Socket, PID, 1, Floor - 1);
-		{get_order_button_status, cab_button, Floor, PID}  when is_integer(Floor) andalso Floor >= 1 andalso Floor =< ?NUMBER_OF_FLOORS andalso is_pid(PID) ->
-			return_order_button_status(Socket, PID, 2, Floor - 1);
+		{get_order_button_status, up_button, Floor, PID}   when is_pid(PID) -> return_order_button_status(Socket, PID, 0, Floor - 1);
+		{get_order_button_status, down_button, Floor, PID} when is_pid(PID) -> return_order_button_status(Socket, PID, 1, Floor - 1);
+		{get_order_button_status, cab_button, Floor, PID}  when is_pid(PID) -> return_order_button_status(Socket, PID, 2, Floor - 1);
+
+		{get_floor_status, PID} when is_pid(PID) -> return_floor_status(Socket, PID);
+
+		turn_off_all_leds -> turn_off_all_order_LEDs(1);
 		
-		{get_stop_button_status, PID} when is_pid(PID) ->
-			return_stop_button_status(Socket, PID);
-
-		{get_floor, PID} when is_pid(PID) ->
-			return_floor_status(Socket, PID);
-
-		turn_off_all_leds ->
-			turn_off_leds_at_floor(1);
-		
-		Unexpected ->
-			io:format("Unexpected message in main_loop of driver module: ~p~n", [Unexpected])
+		Unexpected -> io:format("Unexpected message in main_loop of driver module: ~p~n", [Unexpected])
 
 	end, 
 	main_loop(Socket).
 
 
-%--------------------------------------------------------------------------------------------------
-% Help functions taking care of communication with the elevator driver.
-% The return values are sent back to the process asking for information.
-%--------------------------------------------------------------------------------------------------
 
-return_order_button_status(Socket, PID, Button_type, Floor) when is_pid(PID) andalso is_integer(Button_type) andalso is_integer(Floor) andalso Floor >= 0 andalso Floor < ?NUMBER_OF_FLOORS ->
+%--------------------------------------------------------------------------------------------------
+% Checks if the button 'Button_type' at floor 'Floor' is pressed and sends the response to 'PID'
+%--------------------------------------------------------------------------------------------------
+return_order_button_status(Socket, PID, Button_type, Floor) ->
 	gen_tcp:send(Socket, [6, Button_type, Floor, 0]),
 		case gen_tcp:recv(Socket, ?MSG_LENGTH, ?TIMEOUT) of
 			{ok, [6, Is_pressed, 0, 0]} ->
-				% The elevator server returns button type as a zero indexed integer but we want an atom
+				% Buttoon type must be converted from (zero-based) integer to atom used elsewhere
 				Button_type_atom = element(Button_type + 1, {up_button, down_button, cab_button}),
 				PID ! {order_button_status, Button_type_atom, Floor + 1, Is_pressed};
 			{error, Reason} ->
 				PID ! {error, Reason}
 		end.
 
-return_floor_status(Socket, PID) when is_pid(PID) ->
+
+
+%--------------------------------------------------------------------------------------------------
+% Samples the floor sensor and sends its status to 'PID'
+%--------------------------------------------------------------------------------------------------
+return_floor_status(Socket, PID) ->
 	gen_tcp:send(Socket, [7, 0, 0, 0]),
 		case gen_tcp:recv(Socket, ?MSG_LENGTH, ?TIMEOUT) of
 			{ok, [7, 0, _Latest_floor, 0]} ->
@@ -106,29 +92,28 @@ return_floor_status(Socket, PID) when is_pid(PID) ->
 				PID ! {error, Reason}
 		end.
 
-return_stop_button_status(Socket, PID) when is_pid(PID) ->
-	gen_tcp:send(Socket, [8, 0, 0, 0]),
-		case gen_tcp:recv(Socket, ?MSG_LENGTH, ?TIMEOUT) of
-			{ok, [8, Is_pressed, 0, 0]} ->
-				PID ! {stop_button_status, Is_pressed};
-			{error, Reason} ->
-				PID ! {error, Reason}
-		end.
 
 
-turn_off_leds_at_floor(1) ->
-    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, 1, off} end, [up_button, cab_button]),
-    turn_off_leds_at_floor(2);
-
-turn_off_leds_at_floor(?NUMBER_OF_FLOORS) -> 
-    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, ?NUMBER_OF_FLOORS, off} end, [down_button, cab_button]);
-
-turn_off_leds_at_floor(Floor) when is_integer(Floor) andalso Floor > 1 andalso Floor < ?NUMBER_OF_FLOORS ->
-    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, Floor, off} end, [up_button, down_button, cab_button]),
-    turn_off_leds_at_floor(Floor + 1).
-
-
+%--------------------------------------------------------------------------------------------------
+% Initializes LEDs by turning all off
+%--------------------------------------------------------------------------------------------------
 initialize_LED(Socket) ->
 	gen_tcp:send(Socket, [4, 0, 0, 0]),		% turn off door open LED
 	gen_tcp:send(Socket, [5, 0, 0, 0]),		% turn off stop button LED
-	turn_off_leds_at_floor(1).
+	turn_off_all_order_LEDs(1).				% starts on first floor looping upwards
+
+
+
+%--------------------------------------------------------------------------------------------------
+% Turns off all order button LEDs
+%--------------------------------------------------------------------------------------------------
+turn_off_all_order_LEDs(1) ->
+    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, 1, off} end, [up_button, cab_button]),
+    turn_off_all_order_LEDs(2);
+
+turn_off_all_order_LEDs(?NUMBER_OF_FLOORS) -> 
+    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, ?NUMBER_OF_FLOORS, off} end, [down_button, cab_button]);
+
+turn_off_all_order_LEDs(Floor) when is_integer(Floor) andalso Floor > 1 andalso Floor < ?NUMBER_OF_FLOORS ->
+    lists:foreach(fun(Button_type) -> driver ! {set_order_button_LED, Button_type, Floor, off} end, [up_button, down_button, cab_button]),
+    turn_off_all_order_LEDs(Floor + 1).
