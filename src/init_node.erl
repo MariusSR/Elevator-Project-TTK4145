e90e0b31fb24cc4b72e0aa996329185459d9bccf
«simenkf» <simen_kf@hotmail.com>
2018-04-12 16:46:57 +0200

    Updated comment

--- src/init_node.erl
+++ src/init_node.erl
@@ -19,8 +19,8 @@
 %% The color module is forked from [INSERT SOURCE] and used extensively in most moduels in prints:
 %%      - Red     prints indicate an error or an unexpected message
 %%      - Cyan    prints indicate PIDs of processes spawn from this module
-%%      - Green   prints indicate a node connecting or disconnecting
-%%      - Magenta prints indicate timeouts
+%%      - Green   prints indicate a node connecting
+%%      - Magenta prints indicate timeouts and node disconnecting
 %%      - Yellow  prints indicate change of state in fsm
 %%=================================================================================================
 
