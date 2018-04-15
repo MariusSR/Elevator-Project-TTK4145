1b52cd079b9dd70a711ea6c5eb63ac704842b23e
Marius Rundhovde <Marius.Rundhovde@gmail.com>
2018-04-15 16:05:56 +0200

    Changed some varibales to be shorter

--- src/orders_and_states.erl
+++ src/orders_and_states.erl
@@ -72,8 +72,8 @@ main_loop(Orders, States) ->
         %----------------------------------------------------------------------------------------------
         assign_order_to_fsm ->
             case lists:keyfind(node(), 2, Orders#orders.assigned) of
-                {Already_assigned_order, _Node} ->  % Order already assigned to this node
-                    fsm ! {assigned_order, Already_assigned_order, Orders#orders.cab ++ Orders#orders.unassigned},
+                {Assigned_order, _Node} ->  % Order already assigned to this node
+                    fsm ! {assigned_order, Assigned_order, Orders#orders.cab ++ Orders#orders.unassigned},
                     main_loop(Orders, States);
                 false ->
                     continue
@@ -83,13 +83,13 @@ main_loop(Orders, States) ->
                 [Cab_order|Remaining_cab_orders] ->  % Prioritize cab orders
                     communicator ! {new_order_assigned, Cab_order},
                     fsm          ! {assigned_order, Cab_order, Remaining_cab_orders ++ Orders#orders.unassigned};
-                [] ->  % No cab orders available
+                [] ->  % No cab order available
                     case scheduler:get_most_efficient_order(Orders#orders.unassigned, States) of
                         no_orders_available ->
                             spawn(fun() -> timer:sleep(?RETRY_ASSIGNING_PERIOD), data_manager ! assign_order_to_fsm end);
-                        Hall_order ->
-                            communicator ! {new_order_assigned, Hall_order},
-                            fsm          ! {assigned_order, Hall_order, Orders#orders.cab ++ Orders#orders.unassigned -- [Hall_order]}
+                        Order ->
+                            communicator ! {new_order_assigned, Order},
+                            fsm          ! {assigned_order, Order, Orders#orders.cab ++ Orders#orders.unassigned -- [Order]}
                     end
             end,
             main_loop(Orders, States);
@@ -103,9 +103,9 @@ main_loop(Orders, States) ->
             %{ok, State} = dict:find(Node, States),
             %Updated_states = dict:store(Node, State#state{assigned_order = Order}),
             io:format("~s ORDER = ~p     NODE = ~p\n", [color:redb("MARK ORDER ASSIGNED:"), Order, Node]),
-            io:format("######################################1 ~p\n######################################\n", [States]),
+            io:format("##################1 ~p#################\n", [States]),
             Updated_states = dict:update(Node, fun(Old_state) -> Old_state#state{assigned_order = Order} end, States),
-            io:format("######################################2 ~p\n######################################\n", [States]),
+            io:format("##################2 ~p#################\n", [States]),
             case element(1, Order) of
                 cab_button   -> main_loop(Orders, Updated_states);  % Only assigned hall orders are moved to 'assigned' of 'Orders'
                 _Hall_button -> continue
@@ -115,27 +115,25 @@ main_loop(Orders, States) ->
                 true ->  % Order aldready assigned to some node
                     main_loop(Orders, States);
                 false ->
-                    Updated_assigned_hall_orders   = Orders#orders.assigned   ++ [{Order, Node}],
-                    Updated_unassigned_hall_orders = Orders#orders.unassigned -- [Order],
-                    Updated_orders                 = Orders#orders{unassigned =  Updated_unassigned_hall_orders,
-                                                                     assigned =  Updated_assigned_hall_orders},
+                    Updated_assigned_orders   = Orders#orders.assigned   ++ [{Order, Node}],
+                    Updated_unassigned_orders = Orders#orders.unassigned -- [Order],
+                    Updated_orders            = Orders#orders{unassigned =  Updated_unassigned_orders, assigned = Updated_assigned_orders},
                     watchdog ! {start_watching_order, Order},
-                    fsm      ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_hall_orders},
+                    fsm      ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_orders},
                     main_loop(Updated_orders, Updated_states)
             end;
         
 
 
         %----------------------------------------------------------------------------------------------
-        % Move 'Hall_order' from being assigned back to the list of unassigned 'Orders'.
+        % Move 'Order' from being assigned back to the list of unassigned orders.
         %----------------------------------------------------------------------------------------------
-        {unmark_order_assigned, Hall_order} -> % Happens when an assigned order times out
-            Should_keep_order_in_list      = fun(Assigned_order) -> element(1, Assigned_order) /= Hall_order end,
-            Updated_assigned_hall_orders   = lists:filter(Should_keep_order_in_list, Orders#orders.assigned),
-            Updated_unassigned_hall_orders = [Hall_order] ++ Orders#orders.unassigned,
-            Updated_orders                 = Orders#orders{unassigned = Updated_unassigned_hall_orders,
-                                                             assigned = Updated_assigned_hall_orders},                                                             
-            suspend_fsm_if_order_was_locally_assigned(Hall_order, Orders, Updated_orders),
+        {unmark_order_assigned, Order} -> % Happens when an assigned order times out
+            Should_keep_order_in_list = fun(Assigned_order) -> element(1, Assigned_order) /= Order end,
+            Updated_assigned_orders   = lists:filter(Should_keep_order_in_list, Orders#orders.assigned),
+            Updated_unassigned_orders = [Order] ++ Orders#orders.unassigned,
+            Updated_orders            = Orders#orders{unassigned = Updated_unassigned_orders, assigned = Updated_assigned_orders},
+            suspend_fsm_if_order_was_locally_assigned(Order, Orders, Updated_orders),
             main_loop(Updated_orders, States);
 
 
@@ -146,19 +144,18 @@ main_loop(Orders, States) ->
         {remove_order, {cab_button, Floor}} ->
             Updated_cab_orders = Orders#orders.cab -- [{cab_button, Floor}],
             Updated_orders     = Orders#orders{cab =  Updated_cab_orders},
-            fsm ! {update_order_list, Updated_cab_orders  ++ Updated_orders#orders.unassigned},
+            fsm ! {update_order_list, Updated_cab_orders ++ Updated_orders#orders.unassigned},
             remove_cab_order_from_file(Floor),
             main_loop(Updated_orders, States);
 
         {remove_order, Hall_order} ->
             cancel_order_if_assigned_locally(Hall_order, Orders), % Notifies 'fsm' that the 'Hall_order' is served by perhaps another node
-            Should_keep_order_in_list      = fun(Assigned_order) -> element(1, Assigned_order) /= Hall_order end,
-            Updated_assigned_hall_orders   = lists:filter(Should_keep_order_in_list, Orders#orders.assigned),
-            Updated_unassigned_hall_orders = Orders#orders.unassigned -- [Hall_order],
-            Updated_orders                 = Orders#orders{unassigned =  Updated_unassigned_hall_orders,
-                                                             assigned =  Updated_assigned_hall_orders},
+            Should_keep_order_in_list = fun(Assigned_order) -> element(1, Assigned_order) /= Hall_order end,
+            Updated_assigned_orders   = lists:filter(Should_keep_order_in_list, Orders#orders.assigned),
+            Updated_unassigned_orders = Orders#orders.unassigned -- [Hall_order],
+            Updated_orders            = Orders#orders{unassigned =  Updated_unassigned_orders, assigned = Updated_assigned_orders},
             watchdog ! {stop_watching_order, Hall_order},
-            fsm      ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_hall_orders},
+            fsm      ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_orders},
             main_loop(Updated_orders, States);
 
 
@@ -181,14 +178,13 @@ main_loop(Orders, States) ->
         % Moves orders assigned to 'Node' to the the front of the list of unassigend orders of 'Orders'
         %----------------------------------------------------------------------------------------------
         {node_down, Node} ->
-            Orders_assigned_to_offline_node = lists:filter(fun({_Order, Assigned_node}) -> Assigned_node == Node end, Orders#orders.assigned),
-            Hall_orders_extracted           = lists:map(fun({Order, _Node}) -> Order end, Orders_assigned_to_offline_node),
-            Updated_assigned_hall_orders    = Orders#orders.assigned -- Orders_assigned_to_offline_node,
-            Updated_unassigned_hall_orders  = Hall_orders_extracted ++ Orders#orders.unassigned,
-            Updated_orders                  = Orders#orders{unassigned = Updated_unassigned_hall_orders,
-                                                              assigned = Updated_assigned_hall_orders},
-            lists:foreach(fun(Hall_order) -> watchdog ! {stop_watching_order, Hall_order} end, Hall_orders_extracted),
-            fsm ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_hall_orders},
+            Orders_assigned_to_Node   = lists:filter(fun({_Order, Assigned_node}) -> Assigned_node == Node end, Orders#orders.assigned),
+            Assigned_extracted        = lists:map(fun({Order, _Node}) -> Order end, Orders_assigned_to_Node),
+            Updated_assigned_orders   = Orders#orders.assigned  -- Orders_assigned_to_Node,
+            Updated_unassigned_orders = Assigned_extracted      ++ Orders#orders.unassigned,
+            Updated_orders            = Orders#orders{unassigned = Updated_unassigned_orders, assigned = Updated_assigned_orders},
+            lists:foreach(fun(Hall_order) -> watchdog ! {stop_watching_order, Hall_order} end, Assigned_extracted),
+            fsm ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_orders},
             main_loop(Updated_orders, States);
         
 
@@ -201,25 +197,23 @@ main_loop(Orders, States) ->
             Updated_states = dict:store(New_node, #state{movement = uninitialized, floor = undefined, assigned_order = none}, States),
             main_loop(Orders, Updated_states);
 
-        {existing_data, Updated_assigned_hall_orders, Updated_unassigned_hall_orders, Updated_states} ->
-            fsm ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_hall_orders},
-            lists:foreach(fun({Order, _Node})   -> watchdog     ! {start_watching_order, Order} end, Updated_assigned_hall_orders),
+        {existing_data, Updated_assigned_orders, Updated_unassigned_orders, Updated_states} ->
+            fsm ! {update_order_list, Orders#orders.cab ++ Updated_unassigned_orders},
 
-            Assigned_hall_orders_extracted = lists:map(fun({Order, _Node}) -> Order end, Updated_assigned_hall_orders),
-            lists:foreach(fun(Cab_order)             -> communicator ! {set_order_button_LED, on, Cab_order}             end, Orders#orders.cab),
-            lists:foreach(fun(Assigned_hall_order)   -> communicator ! {set_order_button_LED, on, Assigned_hall_order}   end, Assigned_hall_orders_extracted),
-            lists:foreach(fun(Unassigned_hall_order) -> communicator ! {set_order_button_LED, on, Unassigned_hall_order} end, Updated_unassigned_hall_orders),
-            
+            Assigned_extracted = lists:map(fun({Order, _Node}) -> Order end, Updated_assigned_orders),
+            All_orders         = Assigned_extracted ++ Updated_assigned_orders ++ Orders#orders.cab,
+            lists:foreach(fun(Assigned_order) -> watchdog     ! {start_watching_order, Assigned_order} end, Assigned_extracted),
+            lists:foreach(fun(Order)          -> communicator ! {set_order_button_LED, on, Order}      end, All_orders),
+
             Collision_handler = fun(_Node, State1, _State2) -> State1 end,  % If a key-value pair is present in both dicts, choose the former
-            Merged_states = dict:merge(Collision_handler, States, Updated_states),
-
-            Updated_orders = Orders#orders{assigned = Updated_assigned_hall_orders, unassigned = Updated_unassigned_hall_orders},
+            Merged_states     = dict:merge(Collision_handler, States, Updated_states),
+            Updated_orders    = Orders#orders{assigned = Updated_assigned_orders, unassigned = Updated_unassigned_orders},
             main_loop(Updated_orders, Merged_states);
 
 
 
         Unexpected ->
-            io:format("~s Unexpected message: ~p.\n", [color:red("Orders_and_states:"), Unexpected]),
+            io:format("~s Unexpected message in main_loop: ~p.\n", [color:red("Orders_and_states:"), Unexpected]),
             main_loop(Orders, States)
 
     end.
