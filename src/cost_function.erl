675768fee54b70e85ced3fadcb6f734d52e50336
«simenkf» <simen_kf@hotmail.com>
2018-04-16 18:24:04 +0200

    Remove debug print

--- src/cost_function.erl
+++ src/cost_function.erl
@@ -37,7 +37,6 @@ get_optmial_elevator_for_order([Node|Remaining_nodes_to_evaluate], Order, States
         
         error ->
             io:format("~s States for ~p not present in States.\n", [color:red("Cost_function:"), Node]),
-            io:format("    -> Node, Remaining_nodes, Order, Elevator_stat, Best:  ~p,   ~p,   ~p,   ~p,   ~p\n", [Node, Remaining_nodes_to_evaluate, Order, States, Best]), %REMOVE THIS BEFORE DELIVERY
             get_optmial_elevator_for_order(Remaining_nodes_to_evaluate, Order, States, Best)
     end.
 
